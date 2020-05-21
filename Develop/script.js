8580c7a6ac0520d9e2725eef13b5d495550782a2
Brianna Bullock <brianna.bullock16@gmail.com>
2020-05-20 19:16:46 -0500

    created prompts for each case

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var userArray = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +11,36 @@ function writePassword() {
 
 }
 
+
+function generatePassword () {
+var lengthQuestion =  prompt ("How many characters do you want in the password?");
+console.log("line 16" + " " + lengthQuestion)
+var lowerCase = confirm ("Do you want lowercase letters?");
+console.log(lowerCase)
+var upperCase = confirm ("Do you want uppercase letters?");
+console.log(upperCase)
+var numberQuestion = confirm ("Do you want numbers in your password?");
+console.log(numberQuestion)
+var specialChar = confirm ("Do you want special characters in your password?");
+console.log(specialChar)
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// On button click: 
+// 1. prompt alert length (8-128 characters)
+// 2. prompt alert lowercase
+// 3. prompt alert uppercase
+// 4. prompt alert numbers
+// 5. prompt alert special characters
+// Collect inputs and store
+// Create arrays for lowercase outputs, uppercase, numbers, special characters
+// Randomly pick an alphabet, number, or special character from the array
+// Make a loop to randomly pick letters to the input length from user
+// Concantenate those randomly picked letters, numbers, symbols
+// display within text area with append
+
+
+
+
