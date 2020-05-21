57d9006e0094ecce55d35727ece4a5030d3d3bbb
Brianna Bullock <brianna.bullock16@gmail.com>
2020-05-20 22:38:13 -0500

    created randomizing functions to pull characters, numbers, symbols

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,72 @@
-// Assignment Code
+// Pseudocode
+//  On button click: 
+//     1. prompt alert length (8-128 characters)
+//     2. confirm alert lowercase
+//     3. confirm alert uppercase
+//     4. confirm alert numbers
+//     5. confirm alert special characters
+//  Collect inputs and store
+//  Create arrays for lowercase outputs, uppercase, numbers, special characters
+//  Randomly pick an alphabet, number, or special character from the array
+//  Make a loop to randomly pick letters, symbols, numbers if selected as true out of those arrays to string/array that is the input length from user in #2 above
+//  Concatenate those randomly picked letters, numbers, symbols
+//  Display within text area with append
+
+// Variables
 var generateBtn = document.querySelector("#generate");
-var userArray = [];
+// var lengthArray =  prompt ("How many characters do you want in the password?");
+// var lowerCase = confirm ("Do you want lowercase letters?");
+// var upperCase = confirm ("Do you want uppercase letters?");
+// var numberQuestion = confirm ("Do you want numbers in your password?");
+// // var specialChar = confirm ("Do you want special characters in your password?");
+// var userArray = [lengthArray, lowerCase, upperCase, numberQuestion, specialChar];
+
+
+// Functions
+// array for lowercase characters
+
+function findCharCode() {
+  Math.floor(Math.random() * 26);
+};
+function findRandomLowCase() {
+  String.fromCharCode(findCharCode() + 97);
+};
+function findRandomUpperCase() {
+  String.fromCharCode(findCharCode() + 65);
+};
+function findRandomNum() {
+  Math.floor(Math.random() * 9);
+};
+// Using an array of predefined symbols since no specification of which symbols are appropriate in passwords
+symbolArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ';', ',', '.', '[', ']'];
+
+
+
+
+
+
+
+function userAnswers() {
+  // First Alert: Ask user for number of characters
+  var lengthArray =  prompt ("How many characters do you want in the password?");
+  // Check if the number is between 8 and 128, if true keep prompting for other types of characters, numbers, and symbols in password. If false, alert user to input a number in the correct range
+if (lengthArray >= 8 && lengthArray <= 128) {
+  var lowerCase = confirm ("Do you want lowercase letters?");
+  var upperCase = confirm ("Do you want uppercase letters?");
+  var numberQuestion = confirm ("Do you want numbers in your password?");
+  var specialChar = confirm ("Do you want special characters in your password?");
+} 
+// If false, alert user to input a number in the correct range
+else {
+    alert("You must input a password length of minimum 8 characters and maximum 128 characters.")};
+    userAnswers();
+};
+
+
+function generatePassword() {
+  userAnswers();
+};
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,35 +78,11 @@ function writePassword() {
 }
 
 
-function generatePassword () {
-var lengthQuestion =  prompt ("How many characters do you want in the password?");
-console.log("line 16" + " " + lengthQuestion)
-var lowerCase = confirm ("Do you want lowercase letters?");
-console.log(lowerCase)
-var upperCase = confirm ("Do you want uppercase letters?");
-console.log(upperCase)
-var numberQuestion = confirm ("Do you want numbers in your password?");
-console.log(numberQuestion)
-var specialChar = confirm ("Do you want special characters in your password?");
-console.log(specialChar)
-}
+
+
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// On button click: 
-// 1. prompt alert length (8-128 characters)
-// 2. prompt alert lowercase
-// 3. prompt alert uppercase
-// 4. prompt alert numbers
-// 5. prompt alert special characters
-// Collect inputs and store
-// Create arrays for lowercase outputs, uppercase, numbers, special characters
-// Randomly pick an alphabet, number, or special character from the array
-// Make a loop to randomly pick letters to the input length from user
-// Concantenate those randomly picked letters, numbers, symbols
-// display within text area with append
-
-
-
+generateBtn.addEventListener("click", writePassword)
 
